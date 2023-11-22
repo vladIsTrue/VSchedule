c632c1621cbe0e009e8886733ce23c5ab0ab5f03
vladIsTrue <vladyslav.burtsev@yandex.ru>
2023-11-22 22:18:51 +0300

    add function addTextToRect and some functionality for this class. Now the main required functions have been implemented, but there is room for improvement

--- src/scenecreator/scenecreator.cpp
+++ src/scenecreator/scenecreator.cpp
@@ -2,6 +2,7 @@
 
 
 #include <QDate>
+#include <QLabel>
 #include <QGraphicsRectItem>
 #include <QGraphicsTextItem>
 
@@ -31,42 +32,116 @@ QGraphicsScene* SceneCreator::createScene()
 {
     QGraphicsScene * scene = new QGraphicsScene();
 
-    int i = 0;
+    fillStandardsVec(); // refactor this
 
     while(m_employeesQuery->next())
     {
-        QString name = m_employeesQuery->value(0).toString();
+        QString name        = m_employeesQuery->value(0).toString();
         QDate vacationStart = m_employeesQuery->value(1).toDate();
-        QDate vacationEnd = m_employeesQuery->value(2).toDate();
+        QDate vacationEnd   = m_employeesQuery->value(2).toDate();
 
-        QGraphicsRectItem *item = new QGraphicsRectItem(vacationStart.dayOfYear() * 3, i * 15 + 7.5, (vacationEnd.dayOfYear() - vacationStart.dayOfYear()) * 3, 15);
-        item->setPos(300, 0);
-        // Добавляем прямоугольник на сцену
-        scene->addItem(item);
+        m_standards[vacationStart.month() - 1].currentNumber++;
+        if (vacationEnd.month() != vacationStart.month())
+        {
+            m_standards[vacationEnd.month() - 1].currentNumber++;
+        }
 
-        // ну это говно еще можно подправить попробовать
-        QGraphicsTextItem *text = scene->addText(QString::number(vacationEnd.dayOfYear() - vacationStart.dayOfYear()));
-        text->setPos(vacationStart.dayOfYear() * 3 + 300, i * 15);
+        //create a rectangle with which we visualize the duration of the employee’s vacation
+        QGraphicsRectItem *itemRect = new QGraphicsRectItem(vacationStart.dayOfYear() * m_scale
+                                                        , m_currentRow * m_heightRow + m_heightRow / 2
+                                                        , (vacationEnd.dayOfYear() - vacationStart.dayOfYear()) * m_scale
+                                                        , m_heightRow);
+        itemRect->setPos(m_widthEmployeeName, 0);
+        scene->addItem(itemRect);
 
-        // Создаем текст для отображения имени сотрудника
-        QGraphicsTextItem *nameText = new QGraphicsTextItem(name);
-        nameText->setPos(0, i * 15);
-        scene->addItem(nameText);
+        //place the number of employee vacation days in the center of the rectangle
+        addTextToRect(scene, itemRect, QString::number(vacationEnd.dayOfYear() - vacationStart.dayOfYear()));
 
-        // Устанавливаем всплывающую подсказку для блока отпуска
-        QString toolTip = QString("Start: %1\nEnd: %2").arg(vacationStart.toString()).arg(vacationEnd.toString());
-        item->setToolTip(toolTip);
+        //create a value storing the employee's name
+        QGraphicsTextItem *nameEmployeeItem = new QGraphicsTextItem(name);
+        nameEmployeeItem->setPos(0, m_currentRow * m_heightRow);
+        scene->addItem(nameEmployeeItem);
 
-        ++i;
+        QString toolTip = QString("Начало: %1\nКонец: %2")
+                              .arg(vacationStart.toString())
+                              .arg(vacationEnd.toString());
+
+        itemRect->setToolTip(toolTip);
+
+        ++m_currentRow;
     }
 
-//    for (int i = 0; i < 12; ++i)
-//    {
-//        QGraphicsRectItem *item = new QGraphicsRectItem(30 * i * 3, model.rowCount() * 15 + 7.5, 30 * 3, 15);
-//        item->setPos(300, 0);
-//        //item->setBrush(QBrush(Qt::red)); // Устанавливаем красный цвет
-//        scene->addItem(item);
-//    }
+    scene->addLine(m_widthEmployeeName
+                   , 0
+                   , m_widthEmployeeName
+                   , (m_currentRow + 1) * m_heightRow
+                   , QPen(Qt::black));
+
+    scene->addLine(m_dayOfMounth * m_mounthCount * m_scale + m_widthEmployeeName
+                   , 0
+                   , m_dayOfMounth * m_mounthCount * m_scale + m_widthEmployeeName
+                   , (m_currentRow + 1) * m_heightRow
+                   , QPen(Qt::black));
+
+
+    for (int i = 0; i < m_mounthCount; ++i)
+    {
+        QGraphicsRectItem *itemRect = nullptr;
+
+        auto selectedBrush = m_standards[i].currentNumber > m_standards[i].standardNumber
+                             ? QBrush(Qt::red)
+                             : QBrush(Qt::green);
+
+        itemRect = scene->addRect(m_dayOfMounth * m_scale * i
+                                  , (m_currentRow + 1) * m_heightRow
+                                  , m_dayOfMounth * m_scale
+                                  , m_heightRow
+                                  , QPen(Qt::black)
+                                  , selectedBrush);
+
+        itemRect->setPos(m_widthEmployeeName, 0);
+        addTextToRect(scene, itemRect, m_mounth[i]);
+    }
 
     return scene;
+}
+
+void SceneCreator::fillStandardsVec()
+{
+    while(m_stardardsQuery->next())
+    {
+        if (m_stardardsQuery->value(0).toString() == m_mounth[0])
+            m_standards[0].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[1])
+            m_standards[1].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[2])
+            m_standards[2].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[3])
+            m_standards[3].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[4])
+            m_standards[4].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[5])
+            m_standards[5].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[6])
+            m_standards[6].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[7])
+            m_standards[7].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[8])
+            m_standards[8].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[9])
+            m_standards[9].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[10])
+            m_standards[10].standardNumber = m_stardardsQuery->value(1).toInt();
+        if (m_stardardsQuery->value(0).toString() == m_mounth[11])
+            m_standards[11].standardNumber = m_stardardsQuery->value(1).toInt();
+    }
+}
+
+void SceneCreator::addTextToRect(QGraphicsScene* scene, QGraphicsRectItem* itemRect, QString text)
+{
+    QGraphicsTextItem *textItem = scene->addText(text);
+    qreal x1Rect, y1Rect, widthRect, heightRect;
+    itemRect->rect().getRect(&x1Rect, &y1Rect, &widthRect, &heightRect);
+    textItem->setPos(x1Rect + widthRect / 2 + m_widthEmployeeName - textItem->boundingRect().width() / 2
+                     , y1Rect - heightRect / 2 + m_scale);
 }
--- src/scenecreator/scenecreator.h
+++ src/scenecreator/scenecreator.h
@@ -1,6 +1,7 @@
 #ifndef SCENE_H
 #define SCENE_H
 
+#include <QVector>
 #include <QGraphicsScene>
 #include <QtSql/QSqlQuery>
 
@@ -22,8 +23,29 @@ public:
     QGraphicsScene* createScene();
 
 private:
+    void fillStandardsVec();
+    void addTextToRect(QGraphicsScene* scene, QGraphicsRectItem* itemRect, QString text);
+
     QSqlQuery* m_employeesQuery;
     QSqlQuery* m_stardardsQuery;
+
+    const int m_mounthCount         = 12; // it's bad
+    const int m_heightRow           = 15;
+    const int m_dayOfMounth         = 30;
+    const int m_widthEmployeeName   = 200;
+    const int m_scale               = 2;
+    int m_currentRow                = 0;
+
+    struct
+    {
+        int standardNumber = 0;
+        int currentNumber = 0;
+
+    } m_standards[12]; // it's bad
+
+    const char* m_mounth[12] = {"ЯНВАРЬ", "ФЕВРАЛЬ", "МАРТ", "АПРЕЛЬ",
+                               "МАЙ", "ИЮНЬ", "ИЮЛЬ", "АВГУСТ", "СЕНТЯБРЬ",
+                               "ОКТЯБРЬ", "НОЯБРЬ", "ДЕКАБРЬ"};
 };
 
 #endif //SCENE_H
