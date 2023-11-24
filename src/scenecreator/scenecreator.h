1b2d20dd2b0655598b80d0416ac36db762ab05e0
vladIsTrue <vladyslav.burtsev@yandex.ru>
2023-11-24 18:39:57 +0300

    add docs

--- src/scenecreator/scenecreator.h
+++ src/scenecreator/scenecreator.h
@@ -1,3 +1,10 @@
+/**
+ * @file scene.h
+ * @brief The SceneCreator class declaration.
+ *
+ * This class is used to create a graphical scene using data received from the database
+ */
+
 #ifndef SCENE_H
 #define SCENE_H
 
@@ -8,64 +15,98 @@
 
 #define NUMBEROFMONTHS 12
 
+/**
+ * @brief The QueryCode enum represents different types of queries.
+ *
+ * The QueryCode enumeration is used when calling the setQueryFromDB function.
+ * Responsible for which of the QSqlQuery type variables will contain the query result
+ */
 enum QueryCode
 {
-    EMPLOYEES = 0,
-    STANDARDS,
-    LAST
+    EMPLOYEES = 0, /**< Employees query. */
+    STANDARDS,     /**< Standards query. */
+    LAST           /**< Last query. */
 };
 
 template<typename ostreamT>
-ostreamT& operator << (ostreamT& out, const QueryCode& t)
-{
-    switch(t) {
-    case EMPLOYEES: return (out << "EMPLOYEES");
-    case STANDARDS: return (out << "STANDARDS");
-    case LAST:      return (out << "LAST");
-    }
-    return (out);
-}
+ostreamT& operator << (ostreamT& out, const QueryCode& t);
 
 class SceneCreator : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief SceneCreator constructor.
+     */
     SceneCreator();
+
+    /**
+     * @brief SceneCreator destructor.
+     */
     ~SceneCreator();
 
+    /**
+     * @brief Set a query from the database.
+     * @param query The query string.
+     * @param code The query code.
+     * @return The SQL error if any.
+     *
+     * This function executes a query and stores the result in a variable that corresponds to code
+     */
     QSqlError setQueryFromDB(QString query, QueryCode code);
+
+    /**
+     * @brief Create a graphics scene.
+     * @return The created scene.
+     *
+     * The function is responsible for creating a scene that should display the names of employees on the left,
+     * all months of the year at the bottom, and the employee’s vacation period in the work area
+     *
+     */
     QGraphicsScene* createScene();
 
 private:
+    /**
+     * @brief Fill the standards vector.
+     */
     void fillStandardsVec();
+
+    /**
+     * @brief Add text to a rectangle.
+     * @param scene The graphics scene.
+     * @param itemRect The rectangle item.
+     * @param text The text to add.
+     */
     void addTextToRect(QGraphicsScene* scene, QGraphicsRectItem* itemRect, QString text);
+
+    /**
+     * @brief Draw a horizontal line.
+     * @param scene The graphics scene.
+     */
     void drawLine(QGraphicsScene* scene);
 
-    QSqlQuery* m_employeesQuery;
-    QSqlQuery* m_stardardsQuery;
+    QSqlQuery* m_employeesQuery; /**< The employees query. */
+    QSqlQuery* m_stardardsQuery; /**< The standards query. */
 
     struct
     {
-        int standardNumber = 0;
-        int currentNumber  = 0;
+        int standardNumber = 0; /**< The standard number. */
+        int currentNumber  = 0; /**< The current number. */
 
-    } m_standards[NUMBEROFMONTHS];
+    } m_standards[NUMBEROFMONTHS]; /**< The standards array. */
 
-    int m_currentNumberRows                = 0;
+    int m_currentNumberRows; /**< The current number of rows. */
 
-    static const int m_scale               = 2;
-    static const int m_heightRow           = 14;
+    static const int m_scale;               /**< The scale. */
+    static const int m_heightRow;           /**< The height of a row. */
 
-    static const int m_monthsNumber        = 12;
-    static const int m_dayOfMonth          = 30;
+    static const int m_monthsNumber;        /**< The number of months. */
+    static const int m_dayOfMonth;          /**< The number of days in a month. */
 
-    static const int m_indentation         = 50;
-    static const int m_widthEmployeeName   = 200;
+    static const int m_indentation;         /**< The indentation. */
+    static const int m_widthEmployeeName;   /**< The width of the employee name. */
 
-    const char* m_months[NUMBEROFMONTHS] = {"ЯНВАРЬ", "ФЕВРАЛЬ", "МАРТ", "АПРЕЛЬ",
-                                           "МАЙ", "ИЮНЬ", "ИЮЛЬ", "АВГУСТ", "СЕНТЯБРЬ",
-                                           "ОКТЯБРЬ", "НОЯБРЬ", "ДЕКАБРЬ"};
-
+    const char* m_months[NUMBEROFMONTHS];  /**< The months array. */
 };
 
 #endif //SCENE_H
